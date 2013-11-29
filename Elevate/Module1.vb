c08d616d6dd4792ecc2abb326b9a1b5a26ed64bf
timetraveller92 <timetraveller1992@gmail.com>
2013-11-30 01:11:05 +0530

    Added the Project Files
    Added the project files containing the source code as well as the
    compiled binary executable

---
+++ Elevate/Module1.vb
@@ -0,0 +1,23 @@
+﻿Module Module1
+    Sub Main()
+
+        Dim s() As String = System.Environment.GetCommandLineArgs()
+        If System.Environment.GetCommandLineArgs().Count = 2 Then
+            Dim proc As New ProcessStartInfo
+            proc.UseShellExecute = True
+            proc.WorkingDirectory = Environment.CurrentDirectory
+            proc.FileName = s(1)
+            proc.Verb = "runas"
+
+            Try
+                Process.Start(proc)
+            Catch
+                ' The user refused to allow privileges elevation.
+                ' Do nothing and return directly ...
+                Return
+            End Try
+        Else
+            MsgBox("Usage: " + s(0) + " <your-executable.exe>" + vbNewLine + vbNewLine + "Created by TheTimeTraveller (http:\\imoz.in)")
+        End If
+    End Sub
+End Module
